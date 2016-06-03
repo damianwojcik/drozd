3c58de9338940568ede05b0e65619aea5dd126dc
khamian <khamian@gmail.com>
2016-06-03 10:10:23 +0200

    Poprawki RWD, slider autoplay

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function($){
 
 			items: 1,
 		    loop: true,
-		    autoplay: false,
+		    autoplay: true,
 		    autoplayTimeout: 4000,
 		    autoplayHoverPause: true,
 		    nav: true
